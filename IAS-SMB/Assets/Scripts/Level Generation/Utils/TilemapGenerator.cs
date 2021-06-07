cb5e90580cbcda0c3fc423860470f5e6a5b7b5b7
IQUELFergal <fergal.iquel@gmail.com>
2021-06-07 10:56:48 +0200

    Update player inputs

---
+++ IAS-SMB/Assets/Scripts/Level Generation/Utils/TilemapGenerator.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapGenerator : MonoBehaviour
+{
+    Grid grid;
+    Tilemap[] tilemaps;
+
+    [SerializeField] Tilemap tilemap;
+
+   
+
+    [ContextMenu("Create tilemaps")]
+    public void CreateTilemaps(int n)
+    {
+        if (grid != null)
+        {
+            DestroyImmediate(grid.gameObject);
+        }
+        GameObject obj = new GameObject("Level grid");
+        obj.AddComponent<Grid>();
+        grid = obj.GetComponent<Grid>();
+        grid.cellSize = new Vector3(1, 1, 0);
+        tilemaps = new Tilemap[n];
+        for (int i = 0; i < n; i++)
+        {
+            obj = new GameObject("Tilemap" + i);
+            obj.transform.SetParent(grid.transform);
+            obj.transform.localPosition = new Vector3(0, 0, i * 5);
+            obj.layer = LayerMask.NameToLayer("Ground");
+            obj.AddComponent<Tilemap>();
+            obj.AddComponent<TilemapRenderer>();
+            obj.AddComponent<CompositeCollider2D>();
+            obj.AddComponent<TilemapCollider2D>();
+            obj.GetComponent<TilemapCollider2D>().usedByComposite = true;
+            obj.AddComponent<Rigidbody2D>();
+            obj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            tilemaps[i] = obj.GetComponent<Tilemap>();
+        }
+    }
+
+
+
+    /*[ContextMenu("Generate tilemap")]
+    public void GenerateTilemap()
+    {
+        if (tilemap == null)
+        {
+            Debug.Log("No tilemap was found");
+            return;
+        }
+        ClearTilemaps();
+        LevelGenerator levelGenerator = GetComponent<LevelGenerator>();
+        TileBase[,] level = levelGenerator.ToTileArray(levelGenerator.GenerateLevel());
+        DrawTilemap(level, -levelGenerator.Style.chunkSize.x / 2, -levelGenerator.Style.chunkSize.y / 2);
+    }*/
+
+
+    public void GenerateTilemap(Chunk[] level, int xOffset = 0, int yOffset = 0)
+    {
+        int maxTilemapCount = 0;
+        for (int i = 0; i < level.Length; i++)
+        {
+            if (level[i].TilemapCount > maxTilemapCount) maxTilemapCount = level[i].TilemapCount;
+        }
+        CreateTilemaps(maxTilemapCount);
+        DrawLevel(level, xOffset, yOffset);
+    }
+
+
+
+    /*public void DrawTilemap(TileBase[,] level, int xOffset = 0, int yOffset = 0)
+    {
+        for (int y = 0; y < level.GetLength(1); y++)
+        {
+            for (int x = 0; x < level.GetLength(0); x++)
+            {
+                tilemap.SetTile(new Vector3Int(x + xOffset, y + yOffset, 0), level[x, y]);
+            }
+        }
+    }*/
+
+    public void DrawLevel(Chunk[] level, int xOffset = 0, int yOffset = 0)
+    {
+        for (int i = 0; i < level.Length; i++)
+        {
+            for (int j = 0; j < level[i].TileGrid.Length; j++)
+            {
+                for (int y = 0; y < level[i].TileGrid[j].GetLength(1); y++)
+                {
+                    for (int x = 0; x < level[i].TileGrid[j].GetLength(0); x++)
+                    {
+                        tilemaps[j].SetTile(new Vector3Int(x + xOffset + i* level[i].TileGrid[j].GetLength(0), y + yOffset, 0), level[i].TileGrid[j][x, y]);
+                    }
+                }
+            }
+        }
+    }
+
+
+    [ContextMenu("Clear Tilemap")]
+    void ClearTilemaps()
+    {
+        for (int i = 0; i < tilemaps.Length; i++)
+        {
+            tilemaps[i].ClearAllTiles();
+        }
+    }
+
+
+}
--- IAS-SMB/Assets/Scripts/Player/PlayerController.cs
+++ IAS-SMB/Assets/Scripts/Player/PlayerController.cs
@@ -2,22 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5;
-    public float jumpForce = 5;
-    public bool godMode = false;
-    public bool isGrounded = false;
-    public LayerMask mask;
-    private Vector2 movementInput;
+    [Header("Movement settings")]
+    [SerializeField] float speed = 5;
+    private float currentSpeed;
+    [SerializeField] bool godMode = false;
+    [SerializeField] Vector2 movementInput;
+    [SerializeField] float speedSmoothTime = 0.1f;
+    private float speedSmoothVelocity;
+    [SerializeField] float lastMove = 1;
+
+    [Header("Ground detection settings")]
+    [SerializeField] Vector2 groundBoxSize = Vector3.one;
+    [SerializeField] Vector2 groundBoxOffset = Vector3.zero;
+    [SerializeField] LayerMask groundMask;
+    private bool isGrounded = false;
+    private bool wasGrounded = false;
+
+    [Header("Jump settings")]
+    [SerializeField] float baseJumpHeight = 1;
+    [SerializeField] float extraChargeJumpHeight = 2;
+    [SerializeField] float maxJumpChargeTime = 3;
+    [SerializeField] float coyoteTime = 0.2f;
+    private float currentCoyoteTimeLeft = 0;
+    private bool canJump = false;
+    private bool instantJump = false;
+    private bool chargingJump = false;
+    private bool jumpWasCharging = false;
+    [Range(0, 1)]
+    [SerializeField] float airControlPercent = 0.4f;
+    private float jumpChargeTime = 0;
+
+    [SerializeField] float fallMultiplier = 2;
+
+    [Header("Head collision settings")]
+    [SerializeField] Vector2 headBoxSize = Vector3.one;
+    [SerializeField] Vector2 headBoxOffset = Vector3.zero;
+    [SerializeField] LayerMask headMask;
+
+    [Header("Particles effects")]
+    [SerializeField] ParticleSystem footstep;
+    private ParticleSystem.EmissionModule footEmission;
+    [SerializeField] ParticleSystem impactEffect;
+
     private Rigidbody2D rb;
     private BoxCollider2D col;
+    private Animator anim;
+    private SpriteRenderer sr;
 
-    private void Awake()
+    [Header("Events")]
+    
+    [SerializeField] GameEvent OnPlayerHealthUpEvent;
+    [SerializeField] GameEvent OnPlayerDeathEvent;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<BoxCollider2D>();
+        anim = GetComponentInChildren<Animator>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        footEmission = footstep.emission;
     }
 
     public void OnMoveInput(InputAction.CallbackContext context)
@@ -27,27 +75,25 @@ public class PlayerController : MonoBehaviour
 
     public void OnJumpInput(InputAction.CallbackContext context)
     {
-        if (isGrounded)
+        if (context.performed)
         {
-            if (context.started)
+            if (context.interaction is TapInteraction)
             {
-                //Debug.Log("Jump pushed down");
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                instantJump = true;
             }
-            /*if (context.performed)
+            if (context.interaction is HoldInteraction)
             {
-                Debug.Log("Jump pressed");
+                chargingJump = true;
             }
-            if (context.canceled)
-            {
-                Debug.Log("Jump released");
-            }*/
+        }
+        else if (context.canceled && chargingJump)
+        {
+            chargingJump = false;
         }
     }
 
     public void OnEnableGodModeInput(InputAction.CallbackContext context)
     {
-
         if (context.performed)
         {
             godMode = true;
@@ -56,26 +102,82 @@ public class PlayerController : MonoBehaviour
         {
             godMode = false;
         }
-        UpdateCollisions(godMode);
+        HandleCollisions(godMode);
     }
 
     private void Update()
     {
+        //Grounding
         CheckGround();
-        
+        if (isGrounded) currentCoyoteTimeLeft = coyoteTime;
+        else currentCoyoteTimeLeft -= Time.deltaTime;
+
+        canJump = isGrounded || currentCoyoteTimeLeft > 0;
+
+        //Footstep particles
+        if (movementInput.x != 0 && isGrounded)
+        {
+            footEmission.rateOverTime = 35f;
+        }
+        else
+        {
+            footEmission.rateOverTime = 0;
+        }
+
+        //Movement
         if (godMode)
         {
             rb.velocity = movementInput * speed;
         }
-        else rb.velocity = new Vector2(movementInput.x * speed, rb.velocity.y);
+        else
+        {
+            HandleMovement();
+            HandleJump();
+        }
+        CheckHeadCollision();
+
+        //Animations
+        if (movementInput!=Vector2.zero) anim.SetBool("Running", true);
+        else anim.SetBool("Running", false);
+
+        if (lastMove < 0) sr.flipX = true;
+        else sr.flipX = false;
+
+        if(movementInput.x != 0) lastMove = movementInput.x;
+
+
+        //Jump impact particles
+        if (!wasGrounded && isGrounded)
+        {
+            impactEffect.gameObject.SetActive(true);
+            impactEffect.Stop();
+            impactEffect.Play();
+        }
+
+        wasGrounded = isGrounded;
     }
 
     void CheckGround()
     {
-        isGrounded = Physics2D.OverlapBox((Vector2)transform.position + 0.5f * Vector2.down, new Vector2(1, 0.05f), 0, mask);
+        isGrounded = Physics2D.OverlapBox((Vector2)transform.position + groundBoxOffset, groundBoxSize, 0, groundMask);
     }
 
-    void UpdateCollisions(bool mode)
+    void CheckHeadCollision()
+    {
+        Collider2D col = Physics2D.OverlapBox((Vector2)transform.position + headBoxOffset, headBoxSize, 0, headMask);
+        if (col)
+        {
+            /*Block block = col.gameObject.GetComponent<Block>();
+            Debug.Log(col.gameObject.name);
+            if (block != null)
+            {
+                Debug.Log("Block");
+                block.BreakBlock();
+            }*/
+        }
+    }
+
+    void HandleCollisions(bool mode)
     {
         if (mode)
         {
@@ -89,6 +191,87 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
+    void HandleMovement()
+    {
+        float targetSpeed = speed * movementInput.x;
+
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+
+        rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+
+        currentSpeed = rb.velocity.x;
+    }
+
+    void HandleJump()
+    {
+        if (canJump)
+        {
+            if (instantJump)
+            {
+                Jump(0);
+                instantJump = false;
+            }
+            else if (chargingJump)
+            {
+                jumpChargeTime += Time.deltaTime;
+                jumpWasCharging = true;
+
+                if (jumpChargeTime > maxJumpChargeTime)
+                {
+                    jumpChargeTime = maxJumpChargeTime;
+                    chargingJump = false;
+                }
+            }
+            else if (jumpWasCharging && !chargingJump)
+            {
+                Jump(jumpChargeTime);
+                jumpChargeTime = 0;
+                jumpWasCharging = false;
+            }
+        }
+    }
+
+    void Jump(float chargedTime)
+    {
+        float jumpVelocity = Mathf.Sqrt(-2 * -10 * (baseJumpHeight + (chargedTime / maxJumpChargeTime * extraChargeJumpHeight)));
+        rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+    }
+
+    float GetModifiedSmoothTime(float smoothTime)
+    {
+        if (isGrounded)
+        {
+            return smoothTime;
+        }
+        if (airControlPercent == 0)
+        {
+            return float.MaxValue;
+        }
+        return smoothTime / airControlPercent;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Coin":
+                Debug.Log("Coin");
+                break;
+            case "Enemy":
+                Debug.Log("Enemy");
+                break;
+            default:
+                Debug.Log("Collision with unknown gameObject");
+                break;
+        }
+    }
+
     private void OnDrawGizmos()
     {
         if (isGrounded)
@@ -96,7 +279,10 @@ public class PlayerController : MonoBehaviour
             Gizmos.color = Color.green;
         }
         else Gizmos.color = Color.red;
-
-        Gizmos.DrawCube((Vector2)transform.position + 0.5f * Vector2.down, new Vector2(1, 0.05f));
+        //Foot box
+        Gizmos.DrawCube((Vector2)transform.position + groundBoxOffset, groundBoxSize);
+        Gizmos.color = Color.red;
+        //Head box
+        Gizmos.DrawCube((Vector2)transform.position + headBoxOffset, headBoxSize);
     }
 }
